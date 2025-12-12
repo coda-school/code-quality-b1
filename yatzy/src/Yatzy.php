49a5442a18cc4887493e2308695080a40c22af5f
Yoan Thirion <yoan.thirion@gmail.com>
2025-12-12 18:37:30 +0100

    refactor: remove chance

--- yatzy/src/Yatzy.php
+++ yatzy/src/Yatzy.php
@@ -180,14 +180,4 @@ class Yatzy
         return $total;
     }
 
-    public static function chance(int $d1, int $d2, int $d3, int $d4, int $d5): int
-    {
-        $total = 0;
-        $total += $d1;
-        $total += $d2;
-        $total += $d3;
-        $total += $d4;
-        $total += $d5;
-        return $total;
-    }
 }
